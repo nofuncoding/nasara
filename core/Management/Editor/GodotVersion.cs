1ffebf90b8dd055d156a537aaf8eaa415b03d4f6
NoFun <nofuncoding@qq.com>
2024-01-25 15:07:04 +0800

    refactor: project structure

---
+++ core/Management/Editor/GodotVersion.cs
@@ -0,0 +1,57 @@
+using Godot;
+using System;
+using Semver;
+
+namespace Nasara.Core.Management.Editor;
+
+[GlobalClass] // Removable?
+public partial class GodotVersion : RefCounted
+{
+    // TODO: Add Platform
+
+    public VersionChannel Channel { get; }
+    public SemVersion Version { get; }
+    public bool Mono { get; }
+    public string Path { get; }
+    public VersionStatus Status { get; }
+
+    public enum VersionChannel
+    {
+        Stable,
+        Unstable
+    }
+
+    public enum VersionStatus
+    {
+        OK,
+        NotFound,
+    }
+
+    // You should use a dir path
+    public GodotVersion(string version, string path, VersionChannel channel=VersionChannel.Stable, bool mono=false)
+    {
+        Version = SemVersion.Parse(version, SemVersionStyles.Strict);
+        Path = path;
+        Status = VersionStatus.OK;
+
+        if (!DirAccess.DirExistsAbsolute(path))
+        {
+            // GD.PushError("Godot Installation Not Found: `", path, "` is Unreachable");
+            Status = VersionStatus.NotFound;
+        }
+
+        Channel = channel;
+        Mono = mono;
+    }
+
+    public GodotVersion(SemVersion version, string path, VersionChannel channel=VersionChannel.Stable, bool mono=false)
+    {
+        Version = version;
+        Path = path;
+        if (!DirAccess.DirExistsAbsolute(path))
+            GD.PushError("Godot Installation Not Found: `", path, "` is Unreachable");
+
+        Channel = channel;
+        Mono = mono;
+    }
+}
