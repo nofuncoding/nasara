1b7a27a0c63229d594776ce0b70b1c5a5a7a91b2
NoFun <nofuncoding@qq.com>
2024-01-29 11:17:06 +0800

    feat: read project by using `ConfigFile`

---
+++ core/Management/Project/ProjectFile.cs
@@ -0,0 +1,68 @@
+using Godot;
+using Semver;
+using System;
+
+namespace Nasara.Core.Management.Project;
+
+// TODO: I dont know how the godot engine process it...
+// Maybe it will check it later.
+
+/// <summary>
+/// Reads the godot project file `project.godot`
+/// </summary>
+public partial class ProjectFile
+{
+    public readonly string Path;
+    
+    /// <summary>
+    /// I found the `ConfigFile` can read project file, so that's it!
+    /// </summary>
+    ConfigFile file;
+
+    public ProjectFile(string path)
+    {
+        if (path.GetFile() != "project.godot")
+            throw new Exception("Invalid project file path");
+    
+        Path = path;
+
+        // load the file
+        file = new();
+        var err = file.Load(Path);
+
+        if (err != Error.Ok)
+            throw new Exception("Invalid project file");
+        
+        int config_ver = (int)file.GetValue("", "config_version");
+        if (config_ver < 5)
+            throw new Exception($"Project is not supported (config_version={config_ver})");
+    }
+
+    public string GetProjectName()
+    {
+        return (string)file.GetValue("application", "config/name");
+    }
+    
+    public SemVersion GetProjectGodotVersion()
+    {
+        string[] features = GetFeatures();
+        string version_raw = features[0]; // FIXME: This is a hard code
+        return SemVersion.Parse(version_raw, SemVersionStyles.OptionalPatch);
+    }
+
+    public string[] GetFeatures()
+    {
+        return (string[])file.GetValue("application", "config/features");
+    }
+
+    public Variant GetValueRaw(string section, string key, Variant @default = default)
+    {
+        return file.GetValue(section, key, @default);
+    }
+
+    ~ProjectFile() // i dont know why it is here
+    {
+        file.Free();
+        file = null;
+    }
+}
