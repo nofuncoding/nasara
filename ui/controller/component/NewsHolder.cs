2367fcf96f7381af2e6d701976f9d84485858ddc
NoFun <nofuncoding@qq.com>
2024-02-06 18:42:34 +0800

    refactor(news): Changed title type to `LinkButton`

--- ui/controller/component/NewsHolder.cs
+++ ui/controller/component/NewsHolder.cs
@@ -6,7 +6,7 @@ namespace Nasara.UI.Component;
 public partial class NewsHolder : PanelContainer
 {
 	[Export]
-	RichTextLabel titleRtl; // TODO: Use `LinkButton` instead
+	LinkButton titleButton;
 	[Export]
 	Label descriptionLabel;
 	[Export]
@@ -16,49 +16,18 @@ public partial class NewsHolder : PanelContainer
 
 	public override void _Ready()
 	{
-		titleRtl.MetaUnderlined = false;
-		titleRtl.MetaClicked += (Variant meta) => { OS.ShellOpen((string)meta); };
-		titleRtl.MetaHoverStarted += (Variant _) => {
-			DisplayServer.CursorSetShape(DisplayServer.CursorShape.PointingHand);
-			titleRtl.MetaUnderlined = true;
-		};
-		titleRtl.MetaHoverEnded += (Variant _) => {
-			DisplayServer.CursorSetShape(DisplayServer.CursorShape.Arrow);
-			titleRtl.MetaUnderlined = false;
-		};
-
-		titleRtl.Clear();
+		titleButton.Text = "";
 		descriptionLabel.Text = "";
 		pubDateLabel.Text = "";
 	}
 
-	public void SetTitle(string title)
-	{
-		titleRtl.AddText(title);
-	}
+	public void SetTitle(string title) => titleButton.Text = title;
+	
+	public void SetDescription(string description) => descriptionLabel.Text = description;
 
-	public void SetDescription(string description)
-	{
-		descriptionLabel.Text = description;
-	}
+	public void SetTexture(ImageTexture texture) => imageRect.Texture = texture;
 
-	public void SetTexture(ImageTexture texture)
-	{
-		imageRect.Texture = texture;
-	}
+	public void SetLink(string link) => titleButton.Uri = link;
 
-	public void SetLink(string link)
-	{
-		var original = titleRtl.GetParsedText();
-		titleRtl.Clear();
-
-		titleRtl.PushMeta(link);
-		titleRtl.AddText(original);
-		titleRtl.Pop();
-	}
-
-	public void SetPubDate(string pubDate)
-	{
-		pubDateLabel.Text = pubDate;
-	}
+	public void SetPubDate(string pubDate) => pubDateLabel.Text = pubDate;
 }
